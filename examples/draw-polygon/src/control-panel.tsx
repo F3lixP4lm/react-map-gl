b5addfafa04e4773ab9c600a827570be3c46ad9d
Felix Palm <Felix.Palm.hma@fh-zwickau.de>
2022-08-22 10:10:17 +0200

    show GeoJSON polygon parameters

--- examples/draw-polygon/src/control-panel.tsx
+++ examples/draw-polygon/src/control-panel.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import area from '@turf/area';
+import GeoJSON from 'geojson';
 
-function ControlPanel(props) {
+interface ControlPanelProps{
+  polygons: GeoJSON.Feature[]
+}
+
+function ControlPanel(props:ControlPanelProps) {
   let polygonArea = 0;
   for (const polygon of props.polygons) {
     polygonArea += area(polygon);
   }
+
+  const showPolygonJSONArray = () => {
+    let polygonJSONArray ="[" + props.polygons.map(shape => {
+      return `{"id": "${shape.id}", "geometry":${JSON.stringify(shape.geometry)}}`
+    })+ "]";
+    alert(polygonJSONArray);
+  };
 
   return (
     <div className="control-panel">
@@ -16,6 +28,9 @@ function ControlPanel(props) {
           square meters
         </p>
       )}
+      <div>
+        <button type="button" onClick={showPolygonJSONArray}>Show coordinates</button>
+      </div>
       <div className="source-link">
         <a
           href="https://github.com/visgl/react-map-gl/tree/7.0-release/examples/draw-polygon"
